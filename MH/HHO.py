996e43a2c93cfe5706d6925f8b228ec1afa32451
José Lemus-Romani <isaaclemusr@gmail.com>
2020-10-25 20:09:56 -0300

    self.soluciones[indexCond11].shape

--- MH/HHO.py
+++ MH/HHO.py
@@ -148,7 +148,7 @@ class HHO(Metaheuristica):
                     indexCond11 = np.intersect1d(np.argwhere(Eabs<0.5),np.argwhere(r<0.5))#Nos entrega los index de las soluciones a las que debemos aplicar la ecu 11
                     #ecu 12
                     y11 = self.soluciones
-                    array_Xm =np.zeros(self.soluciones[indexCond11])
+                    array_Xm =np.zeros(self.soluciones[indexCond11].shape)
                     array_Xm = array_Xm + Xm
 
                     y11[indexCond11] = y10[self.idxMejorSolucion]- np.multiply( E[indexCond11], np.abs( np.multiply( 2*(1-np.random.uniform(low= 0.0, high=1.0, size=indexCond10.shape[0])), y10[self.idxMejorSolucion] )- array_Xm ) )  
