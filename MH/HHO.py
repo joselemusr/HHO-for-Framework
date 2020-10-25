d052394d4b8ce511dc5d55a433c44275a7366cf5
José Lemus-Romani <isaaclemusr@gmail.com>
2020-10-25 20:17:48 -0300

    .shape test

--- MH/HHO.py
+++ MH/HHO.py
@@ -150,7 +150,7 @@ class HHO(Metaheuristica):
                     y11 = self.soluciones
                     array_Xm =np.zeros(self.soluciones[indexCond11].shape)
                     array_Xm = array_Xm + Xm
-                    print(f'y11[self.idxMejorSolucion].shape: {y11[self.idxMejorSolucion]}')
+                    print(f'y11[self.idxMejorSolucion].shape: {y11[self.idxMejorSolucion].shape}')
                     print(f'E[indexCond11].shape: {E[indexCond11].shape}')
                     print(f'np.random.uniform(low= 0.0, high=1.0, size=indexCond11.shape[0]).shape: {np.random.uniform(low= 0.0, high=1.0, size=indexCond11.shape[0]).shape}')
                     print(f'y11[self.idxMejorSolucion].shape: {y11[self.idxMejorSolucion].shape}')
