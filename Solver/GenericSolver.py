11daf41279b0d5eb20f1a781619f74b30b4e496f
José Lemus-Romani <isaaclemusr@gmail.com>
2020-10-25 19:44:10 -0300

    setIteracionActual

--- Solver/GenericSolver.py
+++ Solver/GenericSolver.py
@@ -48,6 +48,7 @@ class GenericSolver:
         
         for i in range(self.mh.getParametros()[MH.NUM_ITER]):
             inicio = datetime.now()
+            self.mh.setIteracionActual(self, i)
             self.mh.realizarBusqueda()
             indicadores = self.mh.getIndicadores()
             self.agente.observarIndicadores(indicadores)
